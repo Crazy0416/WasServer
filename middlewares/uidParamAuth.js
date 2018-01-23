e59d97a48d5d8d8a6a1409f6aa58213c1e16f1e3
Minho_Kim <minmin0416@naver.com>
2018-01-23 15:54:49 +0900

    [Add] Add midddleware that checks equal url params.uid and session uid for Authentication

---
+++ middlewares/uidParamAuth.js
@@ -0,0 +1,17 @@
+module.exports = function uidParamAuth(req, res, next){
+
+    var uid = req.params.uid;
+
+    if(req.session.uid === uid){
+        next();
+    }else{
+        res.status(401).render('error', {
+            message: "인증되지 않은 사용자입니다.",
+            error:{
+                status: 401,
+                stack: "..."
+            }
+        });
+    }
+
+}
