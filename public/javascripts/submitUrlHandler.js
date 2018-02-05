82b6cd1cbc103fcac1247a8f279c491119a04011
Minho_Kim <minmin0416@naver.com>
2018-02-05 18:45:17 +0900

    [Fix] Fix err

--- public/javascripts/submitUrlHandler.js
+++ public/javascripts/submitUrlHandler.js
@@ -2,6 +2,5 @@ function submitUrlHandler(formId, _method, url, callback) {
     var theForm = document.getElementById(formId);
     theForm.method = _method;
     theForm.action = url;
-    theForm.bind('ajax:complete', callback);
-    theForm.submit();
+    theForm.submit(callback);
 }
