692ef19e0614b4656f90a0390086dac08da1a22d
Minho_Kim <minmin0416@naver.com>
2018-02-09 11:14:55 +0900

    [Fix] Fix wrong callback function

--- public/javascripts/submitUrlHandler.js
+++ public/javascripts/submitUrlHandler.js
@@ -2,14 +2,5 @@ function submitUrlHandler(formId, _method, url, callback) {
     var theForm = document.getElementById(formId);
     theForm.method = _method;
     theForm.action = url;
-    $.ajax({
-        url: url,                           // Any URL
-        data: $('#'+formId).serialize(),                 // Serialize the form data
-        success: function (data) {                        // If 200 OK
-            callback(null, data);
-        },
-        error: function (xhr, text, error) {              // If 40x or 50x; errors
-            callback(error, null);
-        }
-    });
+    $('#'+formId).submit();
 }
