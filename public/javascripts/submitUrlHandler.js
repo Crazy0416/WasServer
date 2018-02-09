3c2e1e7c7fc9235f9c4ad7968278598a32039ce9
Minho_Kim <minmin0416@naver.com>
2018-02-09 10:55:29 +0900

    [Change] Change submitUrlHandler js file

--- public/javascripts/submitUrlHandler.js
+++ public/javascripts/submitUrlHandler.js
@@ -2,5 +2,14 @@ function submitUrlHandler(formId, _method, url, callback) {
     var theForm = document.getElementById(formId);
     theForm.method = _method;
     theForm.action = url;
-    $('#'+formId).submit(callback);
+    $.ajax({
+        url: url,                           // Any URL
+        data: $('#'+formId).serialize(),                 // Serialize the form data
+        success: function (data) {                        // If 200 OK
+            callback(null, data);
+        },
+        error: function (xhr, text, error) {              // If 40x or 50x; errors
+            callback(error, null);
+        }
+    });
 }
