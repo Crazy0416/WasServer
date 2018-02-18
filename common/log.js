e7aa698c86128199a7aaeea12b53ec08be3a3914
MinHo Kim <minmin0416@naver.com>
2018-02-18 15:22:51 +0900

    [Fix] Fix syntax err]

---
+++ common/log.js
@@ -0,0 +1,29 @@
+var winston = require('winston');
+const tsFormat = () => (new Date()).toLocaleTimeString();
+const fs = require('fs');
+const logDir = 'log';
+
+if(!fs.existsSync(logDir)){
+    fs.mkdirSync(logDir);
+}
+
+var logging = new (winston.Logger)({
+    transports: [
+        new (winston.transports.Console)({
+            timestamp: tsFormat,
+            level: 'debug',
+            colorize:true
+        }),
+        new (require('winston-daily-rotate-file'))({
+            level: 'debug',
+            filename: `${logDir}/-logs.log`,
+            timestamp: tsFormat,
+            datePattern: 'yyyy-MM-dd',
+            prepend:true,
+            // maxsize: 1000000,
+            // maxFiles: 5
+        })
+    ]
+});
+
+module.exports = logging;
